a2743766002ca6cbf7addd7c35133ec4903e1b7d
tien.blt <buitien747@gmail.com>
2024-10-10 18:06:17 +0700

    add service layer for category

---
+++ src/main/java/com/java/project/service/impl/CategoryService.java
@@ -0,0 +1,52 @@
+package com.java.project.service.impl;
+
+import java.util.List;
+
+import com.java.project.dao.ICategoryDao;
+import com.java.project.dao.impl.CategoryDao;
+import com.java.project.entity.Category;
+import com.java.project.service.ICategoryService;
+
+public class CategoryService implements ICategoryService {
+	ICategoryDao cateDao = new CategoryDao();
+	@Override
+	public int count() {
+		return cateDao.count();
+	}
+
+	@Override
+	public List<Category> findAll(int page, int pagesize) {
+		return cateDao.findAll();
+	}
+
+	@Override
+	public List<Category> findByCategoryname(String catname) {
+		return cateDao.findByCategoryname(catname);
+	}
+
+	@Override
+	public List<Category> findAll() {
+		return cateDao.findAll();
+	}
+
+	@Override
+	public Category findById(int cateid) {
+		return cateDao.findById(cateid);
+	}
+
+	@Override
+	public void delete(int cateid) throws Exception {
+		cateDao.delete(cateid);
+	}
+
+	@Override
+	public void update(Category category) {
+		cateDao.update(category);
+	}
+
+	@Override
+	public void insert(Category category) {
+		cateDao.insert(category);
+	}
+
+}
