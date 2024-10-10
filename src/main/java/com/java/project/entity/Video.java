624beae38ca7fd75b8f7dda2d078472b2c725067
tien.blt <buitien747@gmail.com>
2024-10-10 17:13:43 +0700

    add test class to create table in database

--- src/main/java/com/java/project/entity/Video.java
+++ src/main/java/com/java/project/entity/Video.java
@@ -3,10 +3,16 @@ package com.java.project.entity;
 import java.io.Serializable;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="videos")
+@NamedQuery(name="Video.findAll", query="SELECT v FROM Video v")
 public class Video implements Serializable{
 
 	private static final long serialVersionUID = 1194288729639283020L;
